e7539c89aa12e9f31ea0608a54436e64154e04cc
Lucas Guimarães <guimaraeslucas242@gmail.com>
2024-12-07 12:31:32 -0300

    feat: added bages and app icon

---
+++ Landmark/Views/Badges/RotatedbadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedbadgeSymbol.swift
+//  Landmark
+//
+//  Created by Lucas Matheus Guimarães on 07/12/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
